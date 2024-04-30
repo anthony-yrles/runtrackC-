225ef5fbbf864818262ef5c07bf6c008cd807e0e
Sluchai40 <mabbolos8@gmail.com>
2024-04-30 16:15:35 +0200

    [ADD] Job03 Day02 finished

--- jour02/job03.cpp
+++ jour02/job03.cpp
@@ -3,9 +3,38 @@
 using namespace std;
 
 main() {
-    int i, n, somme = 0;
-    do {
+    // Declare variables
+    int i, n, somme;
+    somme = 0;
+    i = 0;
+
+    // While loop
+    while (i < 4) {
+        // Ask for an integer
         cout << "Donnez un entier: ";
+        // Get the integer
         cin >> n;
-    } while (n <= 0);
+        // Add the integer to the sum
+        somme += n;
+        // Increment i
+        i++;
+    }
+    // Print the sum
+    std::cout << "Somme: " << somme << std::endl;
+
+    // Do-while loop
+    i = 0;
+    do {
+        // Ask for an integer
+        cout << "Donnez un entier: ";
+        // Get the integer
+        cin >> n;
+        // Add the integer to the sum
+        somme += n;
+        // Increment i
+        i++;
+        // Check if i is less than 4
+    } while (i < 4);
+    // Print the sum
+    std::cout << "Somme: " << somme << std::endl;
 }
