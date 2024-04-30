bbcd12cbbc94b34c4a83451ca6479d180c45b59f
Sluchai40 <mabbolos8@gmail.com>
2024-04-30 11:28:14 +0200

    [ADD] finished job00 day01

---
+++ jour01/job00.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int x = 10;
+int y;
+int a;
+int b;
+int c;
+int d;
+int i;
+int n;
+int p;
+
+int main() {
+    a = x + 5;
+    a = x = y + 2;
+    a = x == y;
+    a < b && c < d;
+    i++ * (n+p);
+}
