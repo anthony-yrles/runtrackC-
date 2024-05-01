ca8ac1ad05c18dbe2c38ea09367ca0606bf63cdf
Sluchai40 <mabbolos8@gmail.com>
2024-05-01 12:39:37 +0200

    [MODIFY] add commentary to Job06 and Job07

--- jour03/job06.cpp
+++ jour03/job06.cpp
@@ -5,11 +5,14 @@
 using namespace std;
 
 int main() {
+
+    // Declare variables
     int inputUser[10] {};
     int input;
     int count = 0;
     int supFive = 0;
 
+    // Get user input until 10 integers are entered
     do {
         cout << "Entrez 10 entiers : ";
         cin >> input;
@@ -17,10 +20,13 @@ int main() {
         count++;
     } while (count < 10);
 
+    // Count integers greater or equal to 5
     for (int i : inputUser) {
         if (i >= 5){
             supFive++;
         };
     }
+    
+    // Output result
     cout << "Il y a " << supFive << " entiers supérieurs ou égaux à 5.";
 }
--- jour03/job07.cpp
+++ jour03/job07.cpp
@@ -4,11 +4,14 @@
 using namespace std;
 
 int main() {
+
+    // Declare variables
     int inputUser[10] {};
     int input;
     int count = 0;
     int biggest = 0;
 
+    // Get user input until 10 integers are entered
     do {
         cout << "Entrez 10 entiers : ";
         cin >> input;
@@ -16,10 +19,13 @@ int main() {
         count++;
     } while (count < 10);
 
+    // Find the biggest integer
     for (int i : inputUser) {
         if (i >= biggest){
             biggest = i;
         };
     }
+    
+    // Output result
     cout << "Le plus grand entier est : " << biggest;
 }
