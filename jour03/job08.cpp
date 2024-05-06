01cbfa31957cb50d5fde4c9dd523347bbaf143e7
Sluchai40 <mabbolos8@gmail.com>
2024-05-06 11:27:59 +0200

    [ADD] Job 08 Day 03 finished

--- jour03/job08.cpp
+++ jour03/job08.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
+#include <cstring>  // Pour utiliser strncpy
 
 int main() {
+    // Ask the user for a string
+    std::cout << "Entrez une chaîne de caractères: ";
+    std::string input;
+    std::getline(std::cin, input);  // Read the whole line
 
-    // Declare variables
-    char tab[10] {};
+    // Create a char array to store the string
+    // +1 for the null terminator
+    char tab[100];
 
+    // Copy the string to the array
+    std::strncpy(tab, input.c_str(), sizeof(tab) - 1);
+    tab[sizeof(tab) - 1] = '\0';  // Be sure to add the null terminator
+
+    // Print the array
+    std::cout << "Le tableau contient: " << tab << std::endl;
+
+    return 0;
 }
